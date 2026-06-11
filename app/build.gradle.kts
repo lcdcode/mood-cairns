e95d5cd0989616e71d745240d902c03770739e85
lcdcode <>
2026-06-11 12:50:59 -0500

    v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.lcdcode.moodcairns"
         minSdk = 29
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
         resourceConfigurations.add("en")
         base.archivesName = "mood-cairns-$versionName"
     }
