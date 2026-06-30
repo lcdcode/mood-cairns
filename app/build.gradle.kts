53c8c5851466bc1e81b602b636e798c09f6d63fc
lcdcode <>
2026-06-30 17:38:08 -0500

    update build and changelog for v1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.lcdcode.moodcairns"
         minSdk = 29
         targetSdk = 34
-        versionCode = 6 
-        versionName = "1.0.5"
+        versionCode = 7 
+        versionName = "1.0.6"
         resourceConfigurations.add("en")
         base.archivesName = "mood-cairns-$versionName"
     }
